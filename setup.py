5df35d05196bcd5986fcff02f6993606b26b9bf2
Dyupich <warrior2001@mail.ru>
2022-01-14 13:16:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-TOKEN = "5082628845:AAF7JiCYGgotXq55C_GNLC73RvwhPdE3PiQ"
+TOKEN = "TOKEN"
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
